df4c4f8c4f3b1737e4b458aef9a8ad41f766ce2d
Ricardo Mateus <ricardomateus23@hotmail.com>
2019-09-13 10:30:20 -0300

    criacao do endpoint update da api usuarios

--- src/main/java/com/greenmile/projeto/recource/UsuarioResource.java
+++ src/main/java/com/greenmile/projeto/recource/UsuarioResource.java
@@ -4,12 +4,14 @@ import java.util.List;
 
 import javax.validation.Valid;
 
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -34,6 +36,17 @@ public class UsuarioResource {
 		return usuarios.findAll();
 	}
 
+	@PutMapping("/{id}")
+	public ResponseEntity<Usuario> atualizar(@PathVariable Long id, @Valid @RequestBody Usuario usuario){
+		return usuarios.findById(id).map(record -> 
+						{
+							BeanUtils.copyProperties(usuario, record, "id");
+							usuarios.save(record);
+							return ResponseEntity.ok().body(record);
+						})
+		           .orElse(ResponseEntity.notFound().build());
+	}
+	
 	@GetMapping("/{id}")
 	public ResponseEntity<Usuario> buscar(@PathVariable Long id){
 		return usuarios.findById(id).map(record -> ResponseEntity.ok().body(record))
