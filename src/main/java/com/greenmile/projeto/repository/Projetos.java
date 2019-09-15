90e2db19d49682e4ae52fea8b0ccb8ecaf991918
Ricardo Mateus <ricardomateus23@hotmail.com>
2019-09-15 20:21:14 -0300

    implementando metodo find por projeto ativo

--- src/main/java/com/greenmile/projeto/repository/Projetos.java
+++ src/main/java/com/greenmile/projeto/repository/Projetos.java
@@ -1,9 +1,11 @@
 package com.greenmile.projeto.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.greenmile.projeto.model.Projeto;
 
 public interface Projetos extends JpaRepository<Projeto, Long> {
-
+	List<Projeto> findByAtivoTrue();
 }
