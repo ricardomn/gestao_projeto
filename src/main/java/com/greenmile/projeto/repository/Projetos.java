6a5565d94e55ef324646b0ebd1acf4408462475b
Ricardo Mateus <ricardomateus23@hotmail.com>
2019-09-15 14:56:43 -0300

    criacao dos repositorios projetos e usuarios dos projetos

---
+++ src/main/java/com/greenmile/projeto/repository/Projetos.java
@@ -0,0 +1,9 @@
+package com.greenmile.projeto.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.greenmile.projeto.model.Projeto;
+
+public interface Projetos extends JpaRepository<Projeto, Long> {
+
+}
---
+++ src/main/java/com/greenmile/projeto/repository/UsuariosDosProjetos.java
@@ -0,0 +1,9 @@
+package com.greenmile.projeto.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.greenmile.projeto.model.UsuariosDoProjeto;
+
+public interface UsuariosDosProjetos extends JpaRepository<UsuariosDoProjeto, Long>{
+
+}
