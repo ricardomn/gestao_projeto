2417dcd2cd5f207ec199e1ac3f6bbf9b5e3fad4d
Ricardo Mateus <ricardomateus23@hotmail.com>
2019-09-16 19:38:26 -0300

    configurando springbatchconfig

--- src/main/java/com/greenmile/projeto/batch/SpringBatchConfig.java
+++ src/main/java/com/greenmile/projeto/batch/SpringBatchConfig.java
@@ -8,8 +8,16 @@ import org.springframework.batch.core.launch.support.RunIdIncrementer;
 import org.springframework.batch.item.ItemProcessor;
 import org.springframework.batch.item.ItemReader;
 import org.springframework.batch.item.ItemWriter;
+import org.springframework.batch.item.file.FlatFileItemReader;
+import org.springframework.batch.item.file.LineMapper;
+import org.springframework.batch.item.file.mapping.BeanWrapperFieldSetMapper;
+import org.springframework.batch.item.file.mapping.DefaultLineMapper;
+import org.springframework.batch.item.file.transform.DelimitedLineTokenizer;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
 
 import com.greenmile.projeto.repository.Importacao;
 
@@ -31,4 +39,35 @@ public class SpringBatchConfig {
 			.start(step)
 			.build(); 
 	}
+	
+	@Bean
+	 public FlatFileItemReader<Importacao> itemReader(@Value("$inputCSVFile") Resource resource){
+	 
+	  FlatFileItemReader<Importacao> flatFileItemReader = new FlatFileItemReader<>();
+	  flatFileItemReader.setResource(new ClassPathResource("sample-data.csv"));
+	  flatFileItemReader.setName("ItemReader");
+	  flatFileItemReader.setLinesToSkip(1);
+	  flatFileItemReader.setLineMapper(lineMapper());
+	                flatFileItemReader.setStrict(false);
+	  return flatFileItemReader;
+	 
+	 }
+	 
+	 @Bean
+	 public LineMapper<Importacao> lineMapper() {
+	 
+	  DefaultLineMapper<Importacao> defaultLineMapper = new DefaultLineMapper<>();
+	  DelimitedLineTokenizer lineTokenizer = new DelimitedLineTokenizer();
+	  lineTokenizer.setDelimiter(",");
+	  lineTokenizer.setStrict(false);
+	  lineTokenizer.setNames(new String[] {"id","name","dept","salary"});
+	 
+	  BeanWrapperFieldSetMapper<Importacao> fieldSetMapper = new BeanWrapperFieldSetMapper<>();
+	  fieldSetMapper.setTargetType(Importacao.class);
+	  defaultLineMapper.setLineTokenizer(lineTokenizer);
+	  defaultLineMapper.setFieldSetMapper(fieldSetMapper);
+	 
+	  return defaultLineMapper;
+	 }
+	
 }
